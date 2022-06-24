ec2e881b67003fd50532a2153b7a481ed57c8a0f
ttani03 <ttanigaki03@gmail.com>
2022-06-24 16:42:19 +0900

    Add reload option

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = FastAPI()
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
